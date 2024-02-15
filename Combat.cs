0b5d19177b8726ca936c360248ad31d3a5ca5a3c
Ryan Bennis <rbennis@vertafore.com>
2024-02-14 21:09:22 -0500

    combat update/start of party formation

--- Combat.cs
+++ Combat.cs
@@ -10,14 +10,28 @@ namespace Combat
 
             while (character.HitPoints > 0 && enemy.HitPoints > 0)
             {
-               
-                character.Attack(enemy.ArmorClass, out bool success, out int damage);
-                if (success)
-                {
-                    enemy.HitPoints -= damage;
-                    System.Console.WriteLine($"{character.Name} attacks {enemy.Name} for " + damage + " damage!");
+                System.Console.WriteLine($"{character.Name} has " + character.HitPoints + " hit points left!");
+                System.Console.WriteLine($"{enemy.Name} has " + enemy.HitPoints + " hit points left!");
+
+                if (character.initiative > enemy.initiative)
+                { 
+                    character.Attack(enemy.ArmorClass, out bool success, out int damage);
+                    if (success)
+                    {
+                        enemy.HitPoints -= damage;
+                        System.Console.WriteLine($"{character.Name} attacks {enemy.Name} for " + damage + " damage!");
+                    }
+                    if (enemy.HitPoints > 0)
+                    {
+                        enemy.Attack(character.ArmorClass, out bool success1, out int damage1);
+                        if (success1)
+                        {
+                            character.HitPoints -= damage1;
+                            System.Console.WriteLine($"{enemy.Name} attacks {character.Name} for " + damage1 + " damage!");
+                        }
+                    }
                 }
-                if (enemy.HitPoints > 0)
+                else 
                 {
                     enemy.Attack(character.ArmorClass, out bool success1, out int damage1);
                     if (success1)
@@ -25,17 +39,26 @@ namespace Combat
                         character.HitPoints -= damage1;
                         System.Console.WriteLine($"{enemy.Name} attacks {character.Name} for " + damage1 + " damage!");
                     }
+                    if (character.HitPoints > 0)
+                    {
+                        character.Attack(enemy.ArmorClass, out bool success, out int damage);
+                        if (success)
+                        {
+                            enemy.HitPoints -= damage;
+                            System.Console.WriteLine($"{character.Name} attacks {enemy.Name} for " + damage + " damage!");
+                        }
+                    }
                 }
             }
 
             if (character.HitPoints <= 0)
             {
 
-                character.Die(character.HitPoints, character.IsMonster);
+                character.Die(character.IsMonster, character.Name);
             }
             else
             {
-                enemy.Die(enemy.HitPoints, enemy.IsMonster);
+                enemy.Die( enemy.IsMonster, enemy.Name);
             }
         }
     }
--- Dungeon.cs
+++ Dungeon.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Character;
 using Combat;
 
@@ -23,6 +24,8 @@ namespace DDDungeon
             int randomIndex = new Random().Next(0, playerNames.Length);
 
             Character1 player = new Character1(playerNames[randomIndex], 10, 10, 10, 10, 10, 10, 10, false);
+
+          //  generateParty();
             Console.WriteLine($"Welcome to the Dungeon, {player.Name}!");
             Console.WriteLine("You are now in a room with a monster!");
             while (playerAlive)
@@ -41,6 +44,21 @@ namespace DDDungeon
         }
 
 
+        public Character1[] generateParty()
+        {
+            Character1[] party = new Character1[4];
+            string[] playerNames = { "Wizard", "Fighter", "Rougue","Druid" };
+            for (int i = 0; i < 4; i++)
+            {
+                int randomIndex = new Random().Next(0, playerNames.Length);
+                Character1 player = new Character1(playerNames[randomIndex], 10, 10, 10, 10, 10, 10, 10, false);
+                party[i] = player;
+                
+            }
+            return party;
+
+        }
+
         public async void ecounter(Character1 character)
         {
            List<Character1> enemies = GenerateEnemies();
@@ -57,11 +75,12 @@ namespace DDDungeon
         public List<Character1> GenerateEnemies()
         {
             string[] enemyNames = { "Little Baby Goblin", "Big Daddy Goblin", "Goblin King", "Goblin Queen", "Goblin Prince", "Goblin Princess", "Goblin Knight", "Goblin Wizard", "Goblin Sorcerer", "Goblin Cleric"};
-            int randomIndex = new Random().Next(0, enemyNames.Length);
+            
 
             for (int i = 0; i < 3; i++)
             {
-                Character1 enemy = new Character1(enemyNames[randomIndex], 10, 10, 10, 10, 10, 10, 10, true);
+                int randomIndex = new Random().Next(0, enemyNames.Length);
+                Character1 enemy = new Character1(enemyNames[randomIndex], 10, 10, 10, 10, 10, 1, 10, true);
                 enemies.Add(enemy);
                 Console.WriteLine($"Generated enemy: {enemy.Name}");
             }
